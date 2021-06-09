82b3d640e4201953a36679b8436ba7db726e2c1a
Maria <kauztmaria@gmail.com>
2021-06-09 22:44:27 +0300

    moremissions

---
+++ js/mission.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+$('.dash-slidesh').slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+$('.dash-slidesh1').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  infinite: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+});
